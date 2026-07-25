b60347fb36cd473a5c575e22fd44cd4b631ec6a9
w8knh8kpfw-source <w8knh8kpfw@privaterelay.appleid.com>
2026-07-25 17:43:54 +0500

    Implement CORS support for specific origin
    Added CORS middleware to allow requests from a specific origin.

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "https://vinir.onrender.com"
+    );
+
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS"
+    );
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 
